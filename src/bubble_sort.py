d4a8324245940de280791c179ecbb78ff0761e2c
Rafael <akafael@users.noreply.github.com>
2020-06-09 21:55:58 -0300

    Change coding style to PEP8

--- src/bubble_sort.py
+++ src/bubble_sort.py
@@ -1,28 +1,41 @@
 """
-Bubble Sort Algorithm implentation
+Bubble Sort Algorithm
  @complexity O(n^2)
  @author Akafael
  @ref https://en.wikipedia.org/wiki/Bubble_sort
 """
-#!/usr/bin/env python
 
-def bubbleSort(arr): 
-    n = len(arr) 
-  
-    # Traverse through all array elements 
-    for i in range(n): 
-  
+
+# !/usr/bin/env python
+
+
+def bubble_sort(arr):
+    """ Sort Array Elements using Bubble Sort Algorithm
+    :return:
+
+    Examples:
+    >>> bubble_sort([1])
+    [1]
+    >>> bubble_sort([10,9,8,7,6,5,4,3,2,1])
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    """
+
+    # Traverse through all array elements
+    n = len(arr)
+    for i in range(n):
         # Last i elements are already in place 
-        for j in range(0, n-i-1): 
-  
+        for j in range(0, n - i - 1):
             # traverse the array from 0 to n-i-1 
             # Swap if the element found is greater 
             # than the next element 
-            if arr[j] > arr[j+1] : 
-                arr[j], arr[j+1] = arr[j+1], arr[j] 
-  
-# Driver code to test above 
-arr = [64, 34, 25, 12, 22, 11, 90] 
-  
-bubbleSort(arr) 
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
+
+
+if __name__ == '__main__':
+    # Test Trivial Cases with doctest
+    import doctest
+    doctest.testmod()
 
--- src/ex_phonebill.py
+++ src/ex_phonebill.py
@@ -1,11 +1,13 @@
-'''
-Challenge from past interview
-'''
+"""
+Challenge from first interview
+"""
+
 import re
+
 
 def calc_bill_value(S):
     '''
-    Calculate the Bill value for a fictions phone call register list
+    Calculate the Bill value for a fiction phone call register list
     Length call, phone Number in the format "HH:MM:SS,000-000-000\n"
     Conditions
         1. time less then 5 min : cost 3 cents/s
@@ -15,49 +17,49 @@ def calc_bill_value(S):
     :return:
     '''
 
-    value = 0
+    bill_value = 0
 
-    # Convert Data to (hour,minute,second,phonenumber)
-    regexpdata = r'([0-9][0-9]):([0-9][0-9]):([0-9][0-9]),([0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9])'
-    data = re.findall(regexpdata, S)
+    # Convert Data to (hour,minute,second,phone_number)
+    SPLIT_BILL_PARAMETERS = r'([0-9][0-9]):([0-9][0-9]):([0-9][0-9]),([0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9])'
+    data = re.findall(SPLIT_BILL_PARAMETERS, S)
 
     ddata = {}
     # Store Phone with longest call time
-    phonemaxcalls = ""
-    timemaxcalls = 0
+    phone_longestcall = ""
+    time_maxcalls = 0
     # Get Duration
     for item in data:
         # Convert Time to s
-        timeSeconds = int(item[0]) * 3600 + int(item[1]) * 60 + int(item[2])
+        time_seconds = int(item[0]) * 3600 + int(item[1]) * 60 + int(item[2])
 
-        # Add Time to phonenumber register
+        # Add Time to phone number register
         if item[3] not in ddata:
-            ddata[item[3]] = timeSeconds
+            ddata[item[3]] = time_seconds
         else:
-            ddata[item[3]] += timeSeconds
+            ddata[item[3]] += time_seconds
 
         # Get Longest Call time
-        if ddata[item[3]] > timemaxcalls:
-            phonemaxcalls = item[3]
-            timemaxcalls = ddata[item[3]]
-        # elif ddata[item[3]] == timemaxcalls:
+        if ddata[item[3]] > time_maxcalls:
+            phone_longestcall = item[3]
+            time_maxcalls = ddata[item[3]]
+        # elif ddata[item[3]] == time_maxcalls:
         #     # Check smallest Number in case of tier if the right number is needed to show up in the bill
-        #     if item[3] < phonemaxcalls:
-        #         phonemaxcalls = item[3]
-        #         timemaxcalls = ddata[item[3]]
+        #     if item[3] < phone_longestcall:
+        #         phone_longestcall = item[3]
+        #         time_maxcalls = ddata[item[3]]
 
     # Remove Longest Call time (dont check smallest tier number since the value it dont change de value)
-    del (ddata[phonemaxcalls])
+    del (ddata[phone_longestcall])
 
     # Generate Bill
-    for phonenumber, time in ddata.items():
-        pricecall = 0
+    for phone_number, time in ddata.items():
+        price_call = 0
         if time < 300:
-            pricecall = time * 3
+            price_call = time * 3
         else:
-            pricecall = (time // 60 + 1) * 150
+            price_call = (time // 60 + 1) * 150
 
-        value += pricecall
+        bill_value += price_call
 
     # Return
-    return value
+    return bill_value
